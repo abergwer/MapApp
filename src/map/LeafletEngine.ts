7a029b361d1968b69a90268ab4055798aa789a7d
Asher Bergwerk <A00B505@iai.co.il>
2026-06-07 10:06:31 +0300

    Refactor LeafletEngine to optimize view synchronization during map movements and zooming

--- src/map/LeafletEngine.ts
+++ src/map/LeafletEngine.ts
@@ -12,6 +12,9 @@ export class LeafletEngine implements MapEngine {
       center: options.center,
       zoom: options.zoom,
       zoomControl: true,
+      zoomAnimation: false,
+      fadeAnimation: false,
+      markerZoomAnimation: false,
     });
 
     L.tileLayer(config.LeafletTilesURL, {
@@ -19,9 +22,22 @@ export class LeafletEngine implements MapEngine {
       maxZoom: 19,
     }).addTo(this.map);
 
-    this.map.on('move', () => {
-      this.viewChangeCallback?.(this.getViewState());
-    });
+    let ticking = false;
+
+    const syncView = () => {
+      if (ticking) return;
+      ticking = true;
+      requestAnimationFrame(() => {
+        this.viewChangeCallback?.(this.getViewState());
+        ticking = false;
+      });
+    };
+
+    this.map.on('move', syncView);
+    this.map.on('zoom', syncView);
+    // Guarantee final state is synced after animations settle
+    this.map.on('zoomend', () => this.viewChangeCallback?.(this.getViewState()));
+    this.map.on('moveend', () => this.viewChangeCallback?.(this.getViewState()));
   }
 
   getViewState(): MapViewState {
