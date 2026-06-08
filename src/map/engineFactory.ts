3e266d9b6c8854aae9e7f3c98cac3239a2ebbbb2
Asher Bergwerk <A00B505@iai.co.il>
2026-06-08 10:02:17 +0300

    refactor: update createMapEngine to load engines dynamically on demand

--- src/map/engineFactory.ts
+++ src/map/engineFactory.ts
@@ -1,17 +1,22 @@
 import { selectedMapEngine } from './mapConfig';
 import type { MapEngine } from './MapEngine';
-import { LeafletEngine } from './LeafletEngine';
-import { MapLibreEngine } from './MapLibreEngine';
-import { CesiumEngine } from './CesiumEngine';
 
-export function createMapEngine(): MapEngine {
+// Engines are loaded on demand so that a heavy engine (e.g. Cesium) is only
+// downloaded when it is actually selected, keeping it out of the main bundle.
+export async function createMapEngine(): Promise<MapEngine> {
   switch (selectedMapEngine) {
-    case 'maplibre':
+    case 'maplibre': {
+      const { MapLibreEngine } = await import('./MapLibreEngine');
       return new MapLibreEngine();
-    case 'cesium':
+    }
+    case 'cesium': {
+      const { CesiumEngine } = await import('./CesiumEngine');
       return new CesiumEngine();
+    }
     case 'leaflet':
-    default:
+    default: {
+      const { LeafletEngine } = await import('./LeafletEngine');
       return new LeafletEngine();
+    }
   }
 }
